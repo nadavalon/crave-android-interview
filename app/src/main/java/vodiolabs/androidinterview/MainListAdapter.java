3c40bf640488eed982840ce5352b6d7548f31d1d
Nadav Alon <nadavalon@gmail.com>
2016-03-10 14:39:57 +0200

    this is my version for passing the test Nadav

--- app/src/main/java/vodiolabs/androidinterview/MainListAdapter.java
+++ app/src/main/java/vodiolabs/androidinterview/MainListAdapter.java
@@ -1,21 +1,33 @@
 package vodiolabs.androidinterview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v7.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.HorizontalScrollView;
 
-import java.util.ArrayList;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import java.util.List;
 
 /**
  * Created by idan on 1/13/15.
  */
 public class MainListAdapter extends BaseAdapter {
-
+    View container;
+    long counter = 0;
     private List<List<String>> mItems;
     private Context mContext;
 
@@ -44,8 +56,95 @@ public class MainListAdapter extends BaseAdapter {
         /*
         The method to implement
          */
-      return null;
+        container = parent;
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
+        ViewGroup myLayout = (ViewGroup) view.findViewById(R.id.mylayout);
+
+        List<String> item = mItems.get(position);
+
+        for (int i = 0; i < item.size(); i++) {
+            String url = item.get(i);
+
+            createSubItem(url, myLayout);
+
+        }
+
+        return view;
     }
 
+    public void createSubItem(String url, ViewGroup parent) {
 
+        ImageView imageView = new ImageView(mContext);
+        imageView.setTag(String.valueOf(counter));
+
+        parent.addView(imageView);
+
+        DownloadImage di = new DownloadImage();
+        di.execute(url, String.valueOf(counter));
+        counter++;
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(200, 200));
+    }
+
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+        String tag;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String url = params[0];
+            tag = params[1];
+            try {
+                URL urlObj = new URL(url);
+                InputStream is = urlObj.openStream();
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+            // Decode image size
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeStream(is, null, options);
+
+                final int REQUIRED_SIZE=200;
+                // Find the correct scale value. It should be the power of 2.
+                int scale = 1;
+                while(options.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                        options.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                    scale *= 2;
+                }
+
+                // Decode with inSampleSize
+                BitmapFactory.Options options2 = new BitmapFactory.Options();
+                options2.inSampleSize = scale;
+
+                is = urlObj.openStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(is, null, options2);
+
+                return bitmap;
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+
+            if (bitmap == null || container == null) {
+                return;
+            }
+
+           ImageView im = (ImageView)container.findViewWithTag(tag);
+            if(im != null){
+                im.setImageBitmap(bitmap);
+
+            }
+
+        }
+    }
 }
